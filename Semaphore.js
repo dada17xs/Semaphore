2be2f10956c878458fab2effe7f545b210315010
David <dada17xs@hotmail.com>
2016-10-08 12:01:47 +0200

    First version, the simplest

---
+++ Semaphore.js
@@ -0,0 +1,21 @@
+Semaphore: function Semaphore() {
+	
+}
+
+// take one resource, if the semaphore is occupied, try 10 seconds after
+Semaphore.p = function(sem, callback) {
+	if (sem.sem <= 0) {
+		setTimeout(function() {
+			Semaphore.p(sem, callback);
+		}.bind(this), 500);
+	}
+	else {
+		sem.sem--; // /!\ First, we decrement sem, then we execute the callback
+		callback();
+	}
+};
+
+// free one resource
+Semaphore.v = function(sem) {
+	sem.sem++;
+};
